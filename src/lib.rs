d675c0e50618e4762909058e794b823fcf552530
MikuroXina <ryosukadnak@gmail.com>
2021-12-07 18:50:24 +0900

    Impl Display for ParseError

--- src/lib.rs
+++ src/lib.rs
@@ -21,6 +21,23 @@ pub enum ParseError {
     },
 }
 
+impl std::fmt::Display for ParseError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ParseError::UnknownCommand { line, col } => {
+                write!(f, "unknown command found at line {}, col {}", line, col)
+            }
+            ParseError::ExpectedToken { line, col, message } => write!(
+                f,
+                "expected {}, but not found at line {}, col {}",
+                message, line, col
+            ),
+        }
+    }
+}
+
+impl std::error::Error for ParseError {}
+
 pub type Result<T> = std::result::Result<T, ParseError>;
 
 pub fn parse(source: &str) -> Result<TokenStream> {
