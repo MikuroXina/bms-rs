86f82b06e84b346d03167a28aacd918551d9ec95
MikuroXina <ryosukadnak@gmail.com>
2021-12-08 14:56:01 +0900

    Fix to skip not command

--- src/lex/token.rs
+++ src/lex/token.rs
@@ -76,98 +76,101 @@ pub enum Token<'a> {
 
 impl<'a> Token<'a> {
     pub(crate) fn parse(c: &mut Cursor<'a>) -> Result<Self> {
-        let command = c
-            .next_token()
-            .ok_or_else(|| c.err_expected_token("command"))?;
+        loop {
+            let command = c
+                .next_token()
+                .ok_or_else(|| c.err_expected_token("command"))?;
 
-        Ok(match command.to_uppercase().as_str() {
-            "#PLAYER" => Self::Player(PlayerMode::from(c)?),
-            "#GENRE" => Self::Genre(
-                c.next_token()
-                    .ok_or_else(|| c.err_expected_token("genre"))?,
-            ),
-            "#TITLE" => Self::Title(
-                c.next_token()
-                    .ok_or_else(|| c.err_expected_token("title"))?,
-            ),
-            "#SUBTITLE" => Self::Title(
-                c.next_token()
-                    .ok_or_else(|| c.err_expected_token("subtitle"))?,
-            ),
-            "#ARTIST" => Self::Artist(
-                c.next_token()
-                    .ok_or_else(|| c.err_expected_token("artist"))?,
-            ),
-            "#SUBARTIST" => Self::SubArtist(
-                c.next_token()
-                    .ok_or_else(|| c.err_expected_token("subartist"))?,
-            ),
-            "#DIFFICULTY" => Self::Difficulty(
-                c.next_token()
-                    .ok_or_else(|| c.err_expected_token("difficulty"))?
-                    .parse()
-                    .map_err(|_| c.err_expected_token("integer"))?,
-            ),
-            "#STAEGFILE" => Self::StageFile(
-                c.next_token()
-                    .map(Path::new)
-                    .ok_or_else(|| c.err_expected_token("stage filename"))?,
-            ),
-            "#BANNER" => Self::Banner(
-                c.next_token()
-                    .map(Path::new)
-                    .ok_or_else(|| c.err_expected_token("banner filename"))?,
-            ),
-            "#TOTAL" => Self::Total(
-                c.next_token()
-                    .ok_or_else(|| c.err_expected_token("gauge increase rate"))?,
-            ),
-            "#BPM" => Self::Bpm(c.next_token().ok_or_else(|| c.err_expected_token("bpm"))?),
-            "#PLAYLEVEL" => Self::PlayLevel(
-                c.next_token()
-                    .ok_or_else(|| c.err_expected_token("play level"))?
-                    .parse()
-                    .map_err(|_| c.err_expected_token("integer"))?,
-            ),
-            "#RANK" => Self::Rank(JudgeLevel::from(c)?),
-            wav if wav.starts_with("#WAV") => {
-                let id = command.trim_start_matches("#WAV");
-                let filename = Path::new(
+            break Ok(match command.to_uppercase().as_str() {
+                "#PLAYER" => Self::Player(PlayerMode::from(c)?),
+                "#GENRE" => Self::Genre(
                     c.next_token()
-                        .ok_or_else(|| c.err_expected_token("key audio filename"))?,
-                );
-                Self::Wav(ObjId::from(id, c)?, filename)
-            }
-            bmp if bmp.starts_with("#BMP") => {
-                let id = command.trim_start_matches("#BMP");
-                let filename = Path::new(
+                        .ok_or_else(|| c.err_expected_token("genre"))?,
+                ),
+                "#TITLE" => Self::Title(
                     c.next_token()
-                        .ok_or_else(|| c.err_expected_token("bgi image filename"))?,
-                );
-                Self::Bmp(ObjId::from(id, c)?, filename)
-            }
-            message
-                if message.starts_with('#')
-                    && message.chars().nth(6) == Some(':')
-                    && 9 <= message.len()
-                    && message.len() % 2 == 1 =>
-            {
-                let track = command[1..4]
-                    .parse()
-                    .map_err(|_| c.err_expected_token("[000-999]"))?;
-                let channel = &command[4..6];
-                let message = &command[7..];
-                Self::Message {
-                    track: Track(track),
-                    channel: Channel::from(channel, c)?,
-                    message,
+                        .ok_or_else(|| c.err_expected_token("title"))?,
+                ),
+                "#SUBTITLE" => Self::Title(
+                    c.next_token()
+                        .ok_or_else(|| c.err_expected_token("subtitle"))?,
+                ),
+                "#ARTIST" => Self::Artist(
+                    c.next_token()
+                        .ok_or_else(|| c.err_expected_token("artist"))?,
+                ),
+                "#SUBARTIST" => Self::SubArtist(
+                    c.next_token()
+                        .ok_or_else(|| c.err_expected_token("subartist"))?,
+                ),
+                "#DIFFICULTY" => Self::Difficulty(
+                    c.next_token()
+                        .ok_or_else(|| c.err_expected_token("difficulty"))?
+                        .parse()
+                        .map_err(|_| c.err_expected_token("integer"))?,
+                ),
+                "#STAEGFILE" => Self::StageFile(
+                    c.next_token()
+                        .map(Path::new)
+                        .ok_or_else(|| c.err_expected_token("stage filename"))?,
+                ),
+                "#BANNER" => Self::Banner(
+                    c.next_token()
+                        .map(Path::new)
+                        .ok_or_else(|| c.err_expected_token("banner filename"))?,
+                ),
+                "#TOTAL" => Self::Total(
+                    c.next_token()
+                        .ok_or_else(|| c.err_expected_token("gauge increase rate"))?,
+                ),
+                "#BPM" => Self::Bpm(c.next_token().ok_or_else(|| c.err_expected_token("bpm"))?),
+                "#PLAYLEVEL" => Self::PlayLevel(
+                    c.next_token()
+                        .ok_or_else(|| c.err_expected_token("play level"))?
+                        .parse()
+                        .map_err(|_| c.err_expected_token("integer"))?,
+                ),
+                "#RANK" => Self::Rank(JudgeLevel::from(c)?),
+                wav if wav.starts_with("#WAV") => {
+                    let id = command.trim_start_matches("#WAV");
+                    let filename = Path::new(
+                        c.next_token()
+                            .ok_or_else(|| c.err_expected_token("key audio filename"))?,
+                    );
+                    Self::Wav(ObjId::from(id, c)?, filename)
                 }
-            }
-            unknown => {
-                eprintln!("unknown command found: {:?}", unknown);
-                todo!();
-            }
-        })
+                bmp if bmp.starts_with("#BMP") => {
+                    let id = command.trim_start_matches("#BMP");
+                    let filename = Path::new(
+                        c.next_token()
+                            .ok_or_else(|| c.err_expected_token("bgi image filename"))?,
+                    );
+                    Self::Bmp(ObjId::from(id, c)?, filename)
+                }
+                message
+                    if message.starts_with('#')
+                        && message.chars().nth(6) == Some(':')
+                        && 9 <= message.len()
+                        && message.len() % 2 == 1 =>
+                {
+                    let track = command[1..4]
+                        .parse()
+                        .map_err(|_| c.err_expected_token("[000-999]"))?;
+                    let channel = &command[4..6];
+                    let message = &command[7..];
+                    Self::Message {
+                        track: Track(track),
+                        channel: Channel::from(channel, c)?,
+                        message,
+                    }
+                }
+                comment if !comment.starts_with('#') => continue,
+                unknown => {
+                    eprintln!("unknown command found: {:?}", unknown);
+                    todo!();
+                }
+            });
+        }
     }
 }
 
