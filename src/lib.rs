5835ff88b78385d98d6b5826bb85fc4b370f5b49
MikuroXina <ryosukadnak@gmail.com>
2021-12-08 16:22:39 +0900

    Rename into BmsError and Add conversion

--- src/lib.rs
+++ src/lib.rs
@@ -4,26 +4,32 @@ use self::lex::LexError;
 
 #[non_exhaustive]
 #[derive(Debug, Clone)]
-pub enum ParseError {
+pub enum BmsError {
     LexError(LexError),
 }
 
-impl std::fmt::Display for ParseError {
+impl From<LexError> for BmsError {
+    fn from(e: LexError) -> Self {
+        Self::LexError(e)
+    }
+}
+
+impl std::fmt::Display for BmsError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            ParseError::LexError(lex) => {
+            BmsError::LexError(lex) => {
                 write!(f, "lex error: {}", lex)
             }
         }
     }
 }
 
-impl std::error::Error for ParseError {
+impl std::error::Error for BmsError {
     fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
         match self {
-            ParseError::LexError(lex) => Some(lex),
+            BmsError::LexError(lex) => Some(lex),
         }
     }
 }
 
-pub type Result<T> = std::result::Result<T, ParseError>;
+pub type Result<T> = std::result::Result<T, BmsError>;
