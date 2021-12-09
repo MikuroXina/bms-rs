cddc36d673b84d700789d6a326ee69d56181044d
MikuroXina <ryosukadnak@gmail.com>
2021-12-09 12:58:54 +0900

    Fix to unwrap

--- tests/lal.rs
+++ tests/lal.rs
@@ -7,6 +7,6 @@ use bms_rs::{
 fn test_lal() {
     let source = include_str!("lilith_mx.bms");
     let ts = parse(source).expect("must be parsed");
-    let bms = Bms::from_token_stream(&ts, RngMock([1]));
+    let bms = Bms::from_token_stream(&ts, RngMock([1])).expect("must be parsed");
     eprintln!("{:?}", bms);
 }
