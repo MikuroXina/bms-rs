193165c212157c2e2c7c1789a9f69db0dc9b0e5f
MikuroXina <ryosukadnak@gmail.com>
2021-12-18 11:55:33 +0900

    Add bpm_changes and section_len_changes

--- src/parse/notes.rs
+++ src/parse/notes.rs
@@ -12,11 +12,63 @@ use crate::lex::{
     token::Token,
 };
 
+/// An object to change the BPM of the score.
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct BpmChangeObj {
+    /// The time to begin the change of BPM.
+    pub time: ObjTime,
+    /// The object id of the BPM change which in the [`super::Header`]'s `bpm_changes`.
+    pub id: ObjId,
+}
+
+impl PartialOrd for BpmChangeObj {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for BpmChangeObj {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.time.cmp(&other.time)
+    }
+}
+
+/// An object to change its section length of the score.
+#[derive(Debug, Clone)]
+pub struct SectionLenChangeObj {
+    /// The time to begin the change of section length.
+    pub time: ObjTime,
+    /// The length to be.
+    pub length: f64,
+}
+
+impl PartialEq for SectionLenChangeObj {
+    fn eq(&self, other: &Self) -> bool {
+        self.time == other.time
+    }
+}
+
+impl Eq for SectionLenChangeObj {}
+
+impl PartialOrd for SectionLenChangeObj {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for SectionLenChangeObj {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.time.cmp(&other.time)
+    }
+}
+
 /// The objects set for querying by lane or time.
 #[derive(Debug, Default)]
 pub struct Notes {
     objs: HashMap<ObjId, Obj>,
     ids_by_key: HashMap<Key, BTreeMap<ObjTime, ObjId>>,
+    bpm_changes: BTreeMap<ObjTime, BpmChangeObj>,
+    section_len_changes: BTreeMap<ObjTime, SectionLenChangeObj>,
 }
 
 impl Notes {
@@ -28,6 +80,16 @@ impl Notes {
     /// Returns the iterator having all of the notes sorted by time.
     pub fn all_notes(&self) -> impl Iterator<Item = &Obj> {
         self.objs.values().sorted()
+    }
+
+    /// Returns the bpm change objects.
+    pub fn bpm_changes(&self) -> &BTreeMap<ObjTime, BpmChangeObj> {
+        &self.bpm_changes
+    }
+
+    /// Returns the section len change objects.
+    pub fn section_len_changes(&self) -> &BTreeMap<ObjTime, SectionLenChangeObj> {
+        &self.section_len_changes
     }
 
     /// Adds the new note object to the notes.
@@ -53,6 +115,44 @@ impl Notes {
 
     pub(crate) fn parse(&mut self, token: &Token) -> Result<()> {
         match token {
+            Token::Message {
+                track,
+                channel: Channel::BpmChange,
+                message,
+            } => {
+                let denominator = message.len() as u32 / 2;
+                for (i, (c1, c2)) in message.chars().tuples().into_iter().enumerate() {
+                    let id = c1.to_digit(36).unwrap() * 36 + c2.to_digit(36).unwrap();
+                    if id == 0 {
+                        continue;
+                    }
+                    let obj = (id as u16).try_into().unwrap();
+                    let time = ObjTime::new(track.0, i as u32, denominator);
+                    if self
+                        .bpm_changes
+                        .insert(time, BpmChangeObj { time, id: obj })
+                        .is_some()
+                    {
+                        eprintln!("duplicate bpm change object detected at {:?}", time);
+                    }
+                }
+            }
+            Token::Message {
+                track,
+                channel: Channel::SectionLen,
+                message,
+            } => {
+                let time = ObjTime::new(track.0, 0, 4);
+                let length = message.parse().expect("f64 as section length");
+                assert!(0.0 < length, "section length must be greater than zero");
+                if self
+                    .section_len_changes
+                    .insert(time, SectionLenChangeObj { time, length })
+                    .is_some()
+                {
+                    eprintln!("duplicate bpm change object detected at {:?}", time);
+                }
+            }
             Token::Message {
                 track,
                 channel:
