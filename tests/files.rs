365e894b8ac8380e5ef009283434a0e8383042b3
MikuroXina <ryosukadnak@gmail.com>
2021-12-19 13:19:46 +0900

    Add test_j219

--- tests/files.rs
+++ tests/files.rs
@@ -18,3 +18,11 @@ fn test_nc() {
     let bms = Bms::from_token_stream(&ts, RngMock([1])).expect("must be parsed");
     eprintln!("{:?}", bms);
 }
+
+#[test]
+fn test_j219() {
+    let source = include_str!("J219_7key.bms");
+    let ts = parse(source).expect("must be parsed");
+    let bms = Bms::from_token_stream(&ts, RngMock([1])).expect("must be parsed");
+    eprintln!("{:?}", bms);
+}
