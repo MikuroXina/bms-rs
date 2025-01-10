44fa2bd08451bc9dd2ad38c0617d930f28dd341b
MiyakoMeow <110924386+MiyakoMeow@users.noreply.github.com>
2025-01-10 12:00:24 +0800

    fix(bms/lex): use Cow<'a, str>, for performance? (#99)

--- tests/base_62.rs
+++ tests/base_62.rs
@@ -15,6 +15,10 @@ fn test_not_base_62() {
     let bms = Bms::from_token_stream(&ts, RngMock([1])).expect("must be parsed");
     eprintln!("{:?}", bms);
     assert_eq!(bms.header.wav_files.len(), 1);
+    assert_eq!(
+        bms.header.wav_files.iter().next().unwrap().1,
+        &std::path::Path::new("fuga.wav").to_path_buf()
+    );
 }
 
 #[test]
