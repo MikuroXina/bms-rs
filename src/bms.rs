79f5a6aec47fcfeb911c8d2aa4940265a03fc6bc
MiyakoMeow <110924386+MiyakoMeow@users.noreply.github.com>
2025-01-13 08:45:15 +0800

    refactor: Move bms part in a folder like bmson (#107)

---
+++ src/bms.rs
@@ -0,0 +1,35 @@
+//! The parser module of BMS(.bms/.bme/.bml/.pms) file.
+
+pub mod lex;
+pub mod parse;
+pub mod time;
+
+use thiserror::Error;
+
+use self::{lex::LexError, parse::ParseError};
+
+/// An error occurred when parsing the BMS format file.
+#[non_exhaustive]
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Error)]
+pub enum BmsError {
+    /// An error comes from lexical analyzer.
+    #[error("lex error: {0}")]
+    LexError(LexError),
+    /// An error comes from syntax parser.
+    #[error("parse error: {0}")]
+    ParseError(ParseError),
+}
+
+impl From<LexError> for BmsError {
+    fn from(e: LexError) -> Self {
+        Self::LexError(e)
+    }
+}
+impl From<ParseError> for BmsError {
+    fn from(e: ParseError) -> Self {
+        Self::ParseError(e)
+    }
+}
+
+/// A custom result type for bms-rs.
+pub type Result<T> = std::result::Result<T, BmsError>;
