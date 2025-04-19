42d717dc2365d86a7e9ac6a1544618fe83431563
MikuroXina <10331164+MikuroXina@users.noreply.github.com>
2025-04-19 12:10:37 +0900

    feat!: Add prompting interface

---
+++ src/bms/parse/prompt.rs
@@ -0,0 +1,139 @@
+//! Prompting interface and utilities.
+//!
+//! An object implementing [`PromptHandler`] is required by [`super::Bms::from_token_stream`]. It is used to handle conflicts and prompt workarounds on parsing the BMS file.
+
+use std::path::Path;
+
+use crate::lex::command::ObjId;
+
+use super::{ParseError, Result, header::Bmp};
+
+/// An interface to prompt about handling conflicts on the BMS file.
+pub trait PromptHandler {
+    /// Determines a [`DuplicationWorkaround`] for duplicating conflicts.
+    fn handle_duplication(&mut self, duplication: PromptingDuplication) -> DuplicationWorkaround;
+}
+
+/// It represents that there is a duplicated definition on the BMS file.
+#[derive(Debug, Clone, PartialEq)]
+#[non_exhaustive]
+pub enum PromptingDuplication<'a> {
+    /// BMP definition is duplicated.
+    Bmp {
+        /// Duplicated BMP object id.
+        id: ObjId,
+        /// Existing definition.
+        older: &'a Bmp,
+        /// Incoming definition.
+        newer: &'a Bmp,
+    },
+    /// BPM definition is duplicated.
+    BpmChange {
+        /// Duplicated BPM object id.
+        id: ObjId,
+        /// Existing definition.
+        older: f64,
+        /// Incoming definition.
+        newer: f64,
+    },
+    /// OPTION definition is duplicated.
+    ChangeOption {
+        /// Duplicated OPTION object id.
+        id: ObjId,
+        /// Existing definition.
+        older: &'a str,
+        /// Incoming definition.
+        newer: &'a str,
+    },
+    /// SPEED definition is duplicated.
+    SpacingFactorChange {
+        /// Duplicated SPEED object id.
+        id: ObjId,
+        /// Existing definition.
+        older: f64,
+        /// Incoming definition.
+        newer: f64,
+    },
+    /// SCROLL definition is duplicated.
+    ScrollingFactorChange {
+        /// Duplicated SCROLL object id.
+        id: ObjId,
+        /// Existing definition.
+        older: f64,
+        /// Incoming definition.
+        newer: f64,
+    },
+    /// TEXT is duplicated.
+    Text {
+        /// Duplicated TEXT object id.
+        id: ObjId,
+        /// Existing definition.
+        older: &'a str,
+        /// Incoming definition.
+        newer: &'a str,
+    },
+    /// WAV definition is duplicated.
+    Wav {
+        /// Duplicated WAV object id.
+        id: ObjId,
+        /// Existing definition.
+        older: &'a Path,
+        /// Incoming definition.
+        newer: &'a Path,
+    },
+}
+
+/// A choice to handle the duplicated definition.
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+#[non_exhaustive]
+pub enum DuplicationWorkaround {
+    /// Choose to use the existing one.
+    UseOlder,
+    /// Choose to use the incoming one.
+    UseNewer,
+    /// Choose to interrupt this parsing.
+    Halt,
+}
+
+impl DuplicationWorkaround {
+    pub(crate) fn apply<T>(self, target: &mut T, newer: T) -> Result<()> {
+        match self {
+            DuplicationWorkaround::UseOlder => Ok(()),
+            DuplicationWorkaround::UseNewer => {
+                *target = newer;
+                Ok(())
+            }
+            DuplicationWorkaround::Halt => Err(ParseError::Halted),
+        }
+    }
+}
+
+/// The strategy that always using older ones.
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct AlwaysUseOlder;
+
+impl PromptHandler for AlwaysUseOlder {
+    fn handle_duplication(&mut self, _: PromptingDuplication) -> DuplicationWorkaround {
+        DuplicationWorkaround::UseOlder
+    }
+}
+
+/// The strategy that always using newer ones.
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct AlwaysUseNewer;
+
+impl PromptHandler for AlwaysUseNewer {
+    fn handle_duplication(&mut self, _: PromptingDuplication) -> DuplicationWorkaround {
+        DuplicationWorkaround::UseNewer
+    }
+}
+
+/// The strategy that always halts parsing.
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct AlwaysHalt;
+
+impl PromptHandler for AlwaysHalt {
+    fn handle_duplication(&mut self, _: PromptingDuplication) -> DuplicationWorkaround {
+        DuplicationWorkaround::Halt
+    }
+}
