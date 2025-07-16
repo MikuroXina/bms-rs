3dc2a0945fe413e28e5761d7663452ec6b341501
MiyakoMeow <110924386+MiyakoMeow@users.noreply.github.com>
2025-07-16 22:01:36 +0800

    feat: more comfortable struct

--- src/bms/parse/random.rs
+++ src/bms/parse/random.rs
@@ -8,10 +8,60 @@ use crate::bms::lex::token::{Token, TokenStream};
 /// Parses the control flow of the token.
 pub(super) fn parse_control_flow<'a>(
     token_stream: &'a TokenStream<'a>,
-    rng: impl Rng,
-) -> Result<Vec<&'a Token<'a>>> {
+    mut rng: impl Rng,
+) -> std::result::Result<Vec<&'a Token<'a>>, ParseError> {
     let mut units: Vec<Unit<'a>> = Vec::new();
+    let val = rng.generate(0..=100);
+    let mut error_list = Vec::new();
+    let ast: Vec<Unit<'a>> = build_control_flow_ast(token_stream, &mut error_list);
+    let tokens: Vec<&'a Token<'a>> = parse_control_flow_ast(ast, &mut error_list);
+    Some(tokens).filter(|_| error_list.len() == 0).ok_or(error_list.into_iter().next().unwrap())
+}
+
+fn build_control_flow_ast<'a>(tokens: &'a TokenStream<'a>, error_list: &mut Vec<ParseError>) -> Vec<Unit<'a>> {
+    let mut units: Vec<Unit<'a>> = Vec::new();
+    for token in tokens.iter() {
+        let unit = build_control_flow_ast_step(token);
+        match unit {
+            Ok(Some(unit)) => units.push(unit), 
+            Ok(None) => (),
+            Err(e) => error_list.push(e),
+        }
+    }
+    units
+}
+
+fn build_control_flow_ast_step<'a>(token: &'a Token<'a>) -> std::result::Result<Option<Unit<'a>>, ParseError> {
     todo!()
+}
+
+
+fn parse_control_flow_ast<'a>(ast: Vec<Unit<'a>>, error_list: &mut Vec<ParseError>) -> Vec<&'a Token<'a>> {
+    todo!()
+}
+
+fn parse_control_flow_ast_step<'a>(ast: Unit<'a>) -> std::result::Result<Vec<&'a Token<'a>>, ParseError> {
+    todo!()
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Error)]
+pub enum ControlFlowRule {
+    #[error("unmatched end if")]
+    UnmatchedEndIf,
+    #[error("unmatched end random")]
+    UnmatchedEndRandom,
+    #[error("unmatched end switch")]
+    UnmatchedEndSwitch,
+    #[error("unmatched else if")]
+    UnmatchedElseIf,
+    #[error("unmatched else")]
+    UnmatchedElse,
+}
+
+impl From<ControlFlowRule> for ParseError {
+    fn from(rule: ControlFlowRule) -> Self {
+        ParseError::ViolateControlFlowRule(rule)
+    }
 }
 
 fn is_control_flow_token(token: &Token) -> bool {
@@ -35,7 +85,7 @@ fn is_control_flow_token(token: &Token) -> bool {
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 enum Unit<'a> {
-    Token(Token<'a>),
+    Token(&'a Token<'a>),
     RandomBlock {
         max: u64,
         if_blocks: Vec<IfBlock<'a>>,
