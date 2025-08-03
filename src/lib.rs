56d42566350cbc7707b23e86b34b13aeb84b9536
MiyakoMeow <110924386+MiyakoMeow@users.noreply.github.com>
2025-08-03 18:50:39 +0800

    doc: split default/optional features

--- src/lib.rs
+++ src/lib.rs
@@ -50,9 +50,14 @@
 //!
 //! # Features
 //!
+//! ## Default Features
+//!
 //! - `bmson` feature enables the BMSON format support.
 //! - `serde` feature enables the `serde` support. It supports [`serde::Serialize`] for all the definications in this crate, and [`serde::Deserialize`] for all the result types.
 //! - `rand` feature enables the random number generator support. It supports [`bms::parse::random::rng::RandRng`].
+//!
+//! ## Optional Features
+//!
 //! - `minor-command` feature enables the commands that are almost never used in modern BMS Players.
 //!
 //! # About the format
