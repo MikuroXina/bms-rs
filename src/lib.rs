d960c9b812c94be51e473c3b62120f49f6ab0db1
MiyakoMeow <110924386+MiyakoMeow@users.noreply.github.com>
2025-08-16 22:28:32 +0800

    doc: split

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 //! The BMS format parser.
 //!
 //! Be-Music Source, called BMS for short, is a file format devised by Urao Yane in 1998 for a simulator of the game Beatmania by KONAMI. This describes what and when notes are arranged and its music metadata. It is a plain text file with some "command" lines starting with `#` character.
+//! 
+//! For detailed information about the BMS format and this crate, see `README.md` in [github:MikuroXina/bms-rs](https://github.com/MikuroXina/bms-rs).
 //!
 //! # Usage
 //!
@@ -60,60 +62,6 @@
 //! ## Optional Features
 //!
 //! - `minor-command` feature enables the commands that are almost never used in modern BMS Players.
-//!
-//! # About the format
-//!
-//! ## Command
-//!
-//! Each command starts with `#` character, and other lines will be ignored. Some commands require arguments separated by whitespace character such as spaces or tabs.
-//!
-//! ```text
-//! #PLAYER 1
-//! #GENRE FUGA
-//! #TITLE BAR(^^)
-//! #ARTIST MikuroXina
-//! #BPM 120
-//! #PLAYLEVEL 6
-//! #RANK 2
-//!
-//! #WAV01 hoge.WAV
-//! #WAV02 foo.WAV
-//! #WAV03 bar.WAV
-//!
-//! #00211:0303030303
-//! ```
-//!
-//! ### Header command
-//!
-//! Header commands are used to express the metadata of the music or the definition for note arrangement.
-//!
-//! ### Message command
-//!
-//! Message command starts with `#XXXYY:ZZ...`. `XXX` is the number of the measure, `YY` is the channel of the message, and `ZZ...` is the object id sequence.
-//!
-//! The measure must start from 1, but some player may allow the 0 measure (i.e. Lunatic Rave 2).
-//!
-//! The channel commonly expresses what the lane be arranged the note to.
-//!
-//! The object id is formed by 2-digit of 36-radix (`[0-9a-zA-Z]`) integer. So the sequence length must be an even number. The `00` object id is the special id, expresses the rest (no object lies). The object lies on the position divided equally by how many the object is in the measure. For example:
-//!
-//! ```text
-//! #00211:0303000303
-//! ```
-//!
-//! This will be placed as:
-//!
-//! ```text
-//! 003|--|--------------|
-//!    |  |03            |
-//!    |  |03            |
-//!    |  |              |
-//!    |  |03            |
-//! 002|--|03------------|
-//!    |  |  []  []  []  |
-//!    |()|[]  []  []  []|
-//!    |-----------------|
-//! ```
 
 #![warn(missing_docs)]
 #![deny(rustdoc::broken_intra_doc_links)]
