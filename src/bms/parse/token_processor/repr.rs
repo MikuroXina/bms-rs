e996b91f9e130bab76b2658aeb26a003b982b698
MiyakoMeow <110924386+MiyakoMeow@users.noreply.github.com>
2025-10-10 13:04:29 +0800

    Revert "add: fix #BASE 16/36 warnings"
    This reverts commit ae6701382b5cf48d3444c19da2da0628f5c17cbc.

--- src/bms/parse/token_processor/repr.rs
+++ src/bms/parse/token_processor/repr.rs
@@ -31,15 +31,12 @@ impl TokenProcessor for RepresentationProcessor {
                 .push(format!("#{name} {args}"));
         }
         match name.to_ascii_uppercase().as_str() {
-            "BASE" => match args {
-                "62" => {
-                    self.0.borrow_mut().header.case_sensitive_obj_id = true;
-                }
-                "16" | "36" => {}
-                _ => {
+            "BASE" => {
+                if args != "62" {
                     return Err(ParseWarning::OutOfBase62);
                 }
-            },
+                self.0.borrow_mut().header.case_sensitive_obj_id = true;
+            }
             "LNMODE" => {
                 let mode: u8 = args.parse().map_err(|_| {
                     ParseWarning::SyntaxError("expected integer between 1 and 3".into())
