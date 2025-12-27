c7a50ccdc31a2b9521eab5da6db6c69c9208b24f
MiyakoMeow <110924386+MiyakoMeow@users.noreply.github.com>
2025-12-27 14:43:43 +0800

    chore: extract test_bms_assert_objs

--- tests/bms/mod.rs
+++ tests/bms/mod.rs
@@ -15,3 +15,32 @@ mod prelude_test;
 mod prompt_handlers;
 mod unparse_merge;
 mod unparse_roundtrip;
+
+use bms_rs::bms::prelude::*;
+use pretty_assertions::assert_eq;
+
+/// Parses the BMS source with the given RNG and asserts that the resulting objects match expectations.
+pub fn test_bms_assert_objs(src: &str, rng: impl Rng, expect_objs: Vec<WavObj>) {
+    let LexOutput {
+        tokens,
+        lex_warnings,
+    } = TokenStream::parse_lex(src);
+    assert_eq!(lex_warnings, vec![]);
+
+    let ParseOutput {
+        bms,
+        parse_warnings,
+    } = Bms::from_token_stream::<'_, KeyLayoutBeat, _, _, _>(
+        &tokens,
+        default_config_with_rng(rng).prompter(AlwaysUseNewer),
+    );
+    assert_eq!(parse_warnings, vec![]);
+    let bms = match bms {
+        Ok(b) => b,
+        Err(e) => panic!("parse failed: {:?}", e),
+    };
+    assert_eq!(
+        bms.notes().all_notes().cloned().collect::<Vec<_>>(),
+        expect_objs
+    );
+}
--- tests/bms/nested_random.rs
+++ tests/bms/nested_random.rs
@@ -1,6 +1,5 @@
 use bms_rs::bms::prelude::*;
 use num::BigUint;
-use pretty_assertions::assert_eq;
 
 #[test]
 fn nested_random() {
@@ -42,170 +41,94 @@ fn nested_random() {
     let id55 = ObjId::try_from("55", false).unwrap();
     let id66 = ObjId::try_from("66", false).unwrap();
 
-    let LexOutput {
-        tokens,
-        lex_warnings,
-    } = TokenStream::parse_lex(SRC);
-    assert_eq!(lex_warnings, vec![]);
+    super::test_bms_assert_objs(
+        SRC,
+        RngMock([BigUint::from(1u64)]),
+        vec![
+            WavObj {
+                offset: ObjTime::start_of(1.into()),
+                channel_id: KeyLayoutBeat::new(PlayerSide::Player1, NoteKind::Visible, Key::Key(1))
+                    .to_channel_id(),
+                wav_id: id11,
+            },
+            WavObj {
+                offset: ObjTime::new(1, 1, 4).expect("4 should be a valid denominator"),
+                channel_id: KeyLayoutBeat::new(PlayerSide::Player1, NoteKind::Visible, Key::Key(2))
+                    .to_channel_id(),
+                wav_id: id22,
+            },
+            WavObj {
+                offset: ObjTime::new(1, 1, 4).expect("4 should be a valid denominator"),
+                channel_id: KeyLayoutBeat::new(PlayerSide::Player1, NoteKind::Visible, Key::Key(5))
+                    .to_channel_id(),
+                wav_id: id55,
+            },
+            WavObj {
+                offset: ObjTime::new(1, 3, 4).expect("4 should be a valid denominator"),
+                channel_id: KeyLayoutBeat::new(PlayerSide::Player1, NoteKind::Visible, Key::Key(4))
+                    .to_channel_id(),
+                wav_id: id44,
+            },
+        ],
+    );
 
-    {
-        let ParseOutput {
-            bms,
-            parse_warnings,
-        } = Bms::from_token_stream::<'_, KeyLayoutBeat, _, _, _>(
-            &tokens,
-            default_config_with_rng(RngMock([BigUint::from(1u64)])),
-        );
-        assert_eq!(parse_warnings, vec![]);
-        let bms = bms.unwrap();
-        assert_eq!(
-            bms.notes().all_notes().cloned().collect::<Vec<_>>(),
-            vec![
-                WavObj {
-                    offset: ObjTime::start_of(1.into()),
-                    channel_id: KeyLayoutBeat::new(
-                        PlayerSide::Player1,
-                        NoteKind::Visible,
-                        Key::Key(1)
-                    )
+    super::test_bms_assert_objs(
+        SRC,
+        RngMock([BigUint::from(1u64), BigUint::from(2u64)]),
+        vec![
+            WavObj {
+                offset: ObjTime::start_of(1.into()),
+                channel_id: KeyLayoutBeat::new(PlayerSide::Player1, NoteKind::Visible, Key::Key(1))
                     .to_channel_id(),
-                    wav_id: id11,
-                },
-                WavObj {
-                    offset: ObjTime::new(1, 1, 4).expect("4 should be a valid denominator"),
-                    channel_id: KeyLayoutBeat::new(
-                        PlayerSide::Player1,
-                        NoteKind::Visible,
-                        Key::Key(2)
-                    )
+                wav_id: id11,
+            },
+            WavObj {
+                offset: ObjTime::new(1, 1, 4).expect("4 should be a valid denominator"),
+                channel_id: KeyLayoutBeat::new(PlayerSide::Player1, NoteKind::Visible, Key::Key(2))
                     .to_channel_id(),
-                    wav_id: id22,
-                },
-                WavObj {
-                    offset: ObjTime::new(1, 1, 4).expect("4 should be a valid denominator"),
-                    channel_id: KeyLayoutBeat::new(
-                        PlayerSide::Player1,
-                        NoteKind::Visible,
-                        Key::Key(5)
-                    )
+                wav_id: id22,
+            },
+            WavObj {
+                offset: ObjTime::new(1, 2, 4).expect("4 should be a valid denominator"),
+                channel_id: KeyLayoutBeat::new(
+                    PlayerSide::Player1,
+                    NoteKind::Visible,
+                    Key::Scratch(1),
+                )
+                .to_channel_id(),
+                wav_id: id66,
+            },
+            WavObj {
+                offset: ObjTime::new(1, 3, 4).expect("4 should be a valid denominator"),
+                channel_id: KeyLayoutBeat::new(PlayerSide::Player1, NoteKind::Visible, Key::Key(4))
                     .to_channel_id(),
-                    wav_id: id55,
-                },
-                WavObj {
-                    offset: ObjTime::new(1, 3, 4).expect("4 should be a valid denominator"),
-                    channel_id: KeyLayoutBeat::new(
-                        PlayerSide::Player1,
-                        NoteKind::Visible,
-                        Key::Key(4)
-                    )
-                    .to_channel_id(),
-                    wav_id: id44,
-                }
-            ]
-        );
-    }
+                wav_id: id44,
+            },
+        ],
+    );
 
-    {
-        let ParseOutput {
-            bms,
-            parse_warnings,
-        } = Bms::from_token_stream(
-            &tokens,
-            default_config_with_rng(RngMock([BigUint::from(1u64), BigUint::from(2u64)])),
-        );
-        assert_eq!(parse_warnings, vec![]);
-        let bms = bms.unwrap();
-        assert_eq!(
-            bms.notes().all_notes().cloned().collect::<Vec<_>>(),
-            vec![
-                WavObj {
-                    offset: ObjTime::start_of(1.into()),
-                    channel_id: KeyLayoutBeat::new(
-                        PlayerSide::Player1,
-                        NoteKind::Visible,
-                        Key::Key(1)
-                    )
+    super::test_bms_assert_objs(
+        SRC,
+        RngMock([BigUint::from(2u64)]),
+        vec![
+            WavObj {
+                offset: ObjTime::start_of(1.into()),
+                channel_id: KeyLayoutBeat::new(PlayerSide::Player1, NoteKind::Visible, Key::Key(1))
                     .to_channel_id(),
-                    wav_id: id11,
-                },
-                WavObj {
-                    offset: ObjTime::new(1, 1, 4).expect("4 should be a valid denominator"),
-                    channel_id: KeyLayoutBeat::new(
-                        PlayerSide::Player1,
-                        NoteKind::Visible,
-                        Key::Key(2)
-                    )
+                wav_id: id11,
+            },
+            WavObj {
+                offset: ObjTime::new(1, 2, 4).expect("4 should be a valid denominator"),
+                channel_id: KeyLayoutBeat::new(PlayerSide::Player1, NoteKind::Visible, Key::Key(3))
                     .to_channel_id(),
-                    wav_id: id22,
-                },
-                WavObj {
-                    offset: ObjTime::new(1, 2, 4).expect("4 should be a valid denominator"),
-                    channel_id: KeyLayoutBeat::new(
-                        PlayerSide::Player1,
-                        NoteKind::Visible,
-                        Key::Scratch(1)
-                    )
+                wav_id: id33,
+            },
+            WavObj {
+                offset: ObjTime::new(1, 3, 4).expect("4 should be a valid denominator"),
+                channel_id: KeyLayoutBeat::new(PlayerSide::Player1, NoteKind::Visible, Key::Key(4))
                     .to_channel_id(),
-                    wav_id: id66,
-                },
-                WavObj {
-                    offset: ObjTime::new(1, 3, 4).expect("4 should be a valid denominator"),
-                    channel_id: KeyLayoutBeat::new(
-                        PlayerSide::Player1,
-                        NoteKind::Visible,
-                        Key::Key(4)
-                    )
-                    .to_channel_id(),
-                    wav_id: id44,
-                }
-            ]
-        );
-    }
-
-    {
-        let ParseOutput {
-            bms,
-            parse_warnings,
-        } = Bms::from_token_stream(
-            &tokens,
-            default_config_with_rng(RngMock([BigUint::from(2u64)])),
-        );
-        assert_eq!(parse_warnings, vec![]);
-        let bms = bms.unwrap();
-        assert_eq!(
-            bms.notes().all_notes().cloned().collect::<Vec<_>>(),
-            vec![
-                WavObj {
-                    offset: ObjTime::start_of(1.into()),
-                    channel_id: KeyLayoutBeat::new(
-                        PlayerSide::Player1,
-                        NoteKind::Visible,
-                        Key::Key(1)
-                    )
-                    .to_channel_id(),
-                    wav_id: id11,
-                },
-                WavObj {
-                    offset: ObjTime::new(1, 2, 4).expect("4 should be a valid denominator"),
-                    channel_id: KeyLayoutBeat::new(
-                        PlayerSide::Player1,
-                        NoteKind::Visible,
-                        Key::Key(3)
-                    )
-                    .to_channel_id(),
-                    wav_id: id33,
-                },
-                WavObj {
-                    offset: ObjTime::new(1, 3, 4).expect("4 should be a valid denominator"),
-                    channel_id: KeyLayoutBeat::new(
-                        PlayerSide::Player1,
-                        NoteKind::Visible,
-                        Key::Key(4)
-                    )
-                    .to_channel_id(),
-                    wav_id: id44,
-                }
-            ]
-        );
-    }
+                wav_id: id44,
+            },
+        ],
+    );
 }
